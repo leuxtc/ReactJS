6bcf93a968a7ddcd58e932750b4bc98bc8349744
Leo <l3oxon3@gmail.com>
2023-12-19 18:57:12 -0300

    Components

---
+++ projeto-react/src/components/helloWorld.js
@@ -0,0 +1,12 @@
+import Frase from './Frase'
+
+function helloWorld() {
+    return (
+        <div>
+            <Frase/>
+            <h1>Olá mundo</h1>
+        </div>
+    )
+}
+
+export default helloWorld
