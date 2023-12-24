4a34524df47a0ae8b1151e80e4b2f405b3a09ede
Leo <l3oxon3@gmail.com>
2023-12-24 19:34:52 -0300

    Update App.js

--- projeto-react/src/App.js
+++ projeto-react/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
-import AppRoutes from './components/Routes'
+import AppRoutes from './components/pages/Routes'
+import Footer from './components/Footer/Footer'
 
 
 function App() {
   return (
     <div>
       <AppRoutes />
+      <Footer />
     </div>
   )
 }
