52369b9c2ab76ea961588845c98e327a6f8b536d
Leo <l3oxon3@gmail.com>
2023-12-26 20:20:11 -0300

    Finish Home

--- Projeto Costs/costs/src/components/pages/Home.js
+++ Projeto Costs/costs/src/components/pages/Home.js
@@ -1,15 +1,16 @@
 import React from "react"
 import styles from './Home.module.css'
 import savings from "../../img/savings.svg"
+import LinkButton from "../layout/LinkButton"
 
 const Home = () => {
     return (
         <section className={styles.home_container}>
-            <img className={styles.img} src={savings} alt="savings"></img>
+            <img src={savings} alt="savings"></img>
             <div>
                 <h1>Welcome to <span>Costs</span></h1>
                 <p>Start to manage your projects, right now!</p>
-                <a href="./NewProject" className={styles.button}>Make Projects</a>
+                <LinkButton to='/newproject' text='Make a project' />
             </div>
         </section>
     )
